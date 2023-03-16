1edf3664ad5b3bea1bebd73787f2921edc4086b4
jmcollie <jcollie4@gmu.edu>
2023-03-15 23:55:17 -0600

    Adds feature in GH-1.
    Built out an ETL pipeline in `process_data.py` to import, clean, create
    features from csv files: `messages.csv` and `categories.csv`, and load
    the process data to a sqlite database. Added functionality to the
    template methods: `load_data`, `clean_data` and `save_data`.

--- data/process_data.py
+++ data/process_data.py
@@ -1,19 +1,113 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Imports the `messages` and `categories` csv files into
+    DataFrames, performs an inner join, and returns the combined
+    DataFrame.
+    
+    Parameters
+    ----------
+    messages_filepath : str
+        The filepath of the messages.csv file.
+    categories_filepath : str
+        The filepath of the categories.csv file.
+    Returns
+    -------
+    : pandas.DataFrame
+        A DataFrame of the messages and categories
+        datasets joined together.
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return messages.merge(categories, how='inner', on='id')
 
 
 def clean_data(df):
-    pass
+    """
+    Creates a separate column for each unique category in the 
+    `categories` column in `df` with a One-Hot Encoded value 
+    for whether the category is present in the row, drops the
+    `category` column, and removes duplicate rows.
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A DataFrame of the messages and categories datasets merged.
+    
+    Returns
+    -------
+    df : pandas.DataFrame
+        Input parameter `df` concatenated with a DataFrame of categories
+        parsed out of the `categories` column in input parameter `df`,
+        with the `categories` column dropped, and duplicate rows removed.
+    """
+    # Creating a DataFrame from the values in the categories
+    # column of df.
+    categories = df['categories'].str.split(';', expand=True)
+    
+    # Parsing and setting column names of the DataFrame using the first row.
+    first_row = categories.iloc[0]
+    categories.columns = first_row.apply(lambda value: 
+                                         value.split('-')[0]).values
+
+    for column in categories:
+        # Setting each value to be the last character of the string.
+        categories[column] = categories[column].str.slice(start=-1)
+        
+        # Converting column datatype from string to numeric.
+        categories[column] = pd.to_numeric(categories[column])
+        
+    # Dropping the categories column from df.
+    df.drop(columns=['categories'], inplace=True)
+    
+    # Concatenating df and categories.
+    df = pd.concat([df, categories], axis=1)
+    
+    # Dropping duplicated rows.
+    df.drop(index=df[df.duplicated()].index, inplace=True)
+        
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filepath):
+    """
+    Creates a sqlite database using `database_filepath` and loads
+    DataFrame `df` to the created database with a
+    table name of `pipeline`.
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A DataFrame of messages and categories.
+    database_filepath : 
+        The name of the database to save the data to.
 
+    Returns
+    -------
+    None
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('pipeline', engine, index=False)
 
 def main():
+    """
+    Calls `load_data`, `clean_data` and `save_data` methods,
+    if sys.argv does not equal four, an error message is 
+    printed.
+    
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
